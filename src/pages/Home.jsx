4b13497b668779912ae309903a2ffd389eb7491c
Abyab kumar <147365036+Abyab143@users.noreply.github.com>
2023-12-31 18:02:01 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ function Home() {
           <div className="card my-3 text-center">
             <div className="row g-0">
               <div className="col-md-6">
-                <img src={data.imgUrl} className="card_img m-2" alt="..." />
+                <img src={data.imgUrl} className="card_img m-2" alt="https://wallpapers.com/images/featured/indian-army-35e82zy24xfm0pgi.jpg" />
               </div>
               <div
                 className="col-md-6"
